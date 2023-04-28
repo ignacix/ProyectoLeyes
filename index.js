2a21a740d3c459d6cffe4c5ada9c177393bd6013
Ignacio Jerez <82911361+ignacix@users.noreply.github.com>
2023-04-28 18:29:16 -0300

    Se suben archivos

---
+++ index.js
@@ -0,0 +1,34 @@
+function obtenerValores(){
+    let select1 = document.getElementById("opcionesCI");
+
+    let valor1 = select1.value;
+
+    let select2 = document.getElementById("opcionesCR");
+
+    let valor2 = select2.value;
+
+    if(valor1=="aprueba"){
+
+        if(valor2=="aprueba"){
+            document.getElementById("resultado").innerHTML ="El proyecto aprobado por la CI y CR PASA AL PODER EJECUTIVO)";    
+        }
+        else{
+            if(valor2=="rechaza"){
+                document.getElementById("resultado").innerHTML ="El proyecto no puede volver a tratarse en las sesiones de ese año";
+            }            
+            else{
+                document.getElementById("resultado").innerHTML ="Vuelve a la camara de origen";
+            }
+        }
+    }
+    else{
+        document.getElementById("resultado").innerHTML ="Espera un año";
+        console.log("No seleccionaste aprueba");
+    }
+
+    console.log(valor1);
+}
+
+let boton = document.getElementById("miBoton");
+
+boton.addEventListener('click', obtenerValores);
