1aa56e0d941850537a1a67f64904a40ffaf8eae0
ignacix <ignanj8mail.com>
2023-05-05 07:02:46 -0300

    Modificación visual y correción de palabras

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 function obtenerValores(){
     let select1 = document.getElementById("opcionesCI");
-
     let valor1 = select1.value;
-
     let select2 = document.getElementById("opcionesCR");
-
     let valor2 = select2.value;
 
     if(valor1=="aprueba"){
 
         if(valor2=="aprueba"){
-            document.getElementById("resultado").innerHTML ="El proyecto aprobado por la CI y CR PASA AL PODER EJECUTIVO)";    
+            document.getElementById("resultado").innerHTML ="Resultado: El proyecto aprobado por la CI y CR PASA AL PODER EJECUTIVO)";    
         }
         else{
             if(valor2=="rechaza"){
-                document.getElementById("resultado").innerHTML ="El proyecto no puede volver a tratarse en las sesiones de ese año";
+                document.getElementById("resultado").innerHTML ="Resultado: El proyecto no puede volver a tratarse en las sesiones de ese año";
             }            
             else{
-                document.getElementById("resultado").innerHTML ="Vuelve a la camara de origen";
+                document.getElementById("resultado").innerHTML ="Resultado: Vuelve a la camara de origen";
             }
         }
     }
     else{
-        document.getElementById("resultado").innerHTML ="Espera un año";
+        document.getElementById("resultado").innerHTML ="Resultado: Espera un año";
         console.log("No seleccionaste aprueba");
     }
 
